e3f007746569e630b32a98f26f5afba7ea8f22be
Rebecca <schironerebecca@gmail.com>
2026-03-28 12:34:41 +0100

    Ho modificato in src nel main.js collapse

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import "./style.scss"
 // import 'bootstrap/js/dist/alert';
 // import 'bootstrap/js/dist/button';
 // import 'bootstrap/js/dist/carousel';
-// import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/collapse';
 // import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/modal';
 // import 'bootstrap/js/dist/offcanvas';
 // import 'bootstrap/js/dist/popover';
 // import 'bootstrap/js/dist/scrollspy';
